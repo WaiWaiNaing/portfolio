3106e8f5ddb356ce314ed97c8890ddc2e65294bd
Wai Wai Naing <waiwainaing@brain-inter.com>
2020-03-16 21:22:08 +0630

    first commit

---
+++ build/precache-manifest.6938e5517380efec7265f1429e4647a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd351a83e35225237a8200acb62d78f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60dfa436dff8286ac0de",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9349f9cc3e804c485769",
+    "url": "/static/js/2.cdeba0a3.chunk.js"
+  },
+  {
+    "revision": "f48d1a91ae4de4991a6f62458cc48c28",
+    "url": "/static/js/2.cdeba0a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60dfa436dff8286ac0de",
+    "url": "/static/js/main.e80479fe.chunk.js"
+  },
+  {
+    "revision": "180396dc7b55f3660971",
+    "url": "/static/js/runtime-main.6eef9028.js"
+  }
+]);
